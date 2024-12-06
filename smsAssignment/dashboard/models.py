55a0140677a1b9d9ee730bd27cdd4f95e448bb48
AngelLeoKim <torotorokuyakim1011@gmail.com>
2024-12-06 17:19:04 +0800

    feat: added team and event name

--- smsAssignment/dashboard/models.py
+++ smsAssignment/dashboard/models.py
@@ -3,4 +3,16 @@ import os
 from twilio.rest import Client
 from django.utils import timezone
 
-# Create your models here.
+# Define a callable function for the default  time value
+def get_current_time():
+    return timezone.now().time()
+
+
+class Message(models.Model):
+     # Define a 'team_name' field to store the name of the team, with a default value.
+    team_name = models.CharField(max_length=100, default='Default Team')  # Provide a default value for team_name
+
+    # Define an 'event_name' field to store the name of the event.
+    event_name = models.CharField(max_length=100, default='Default Team')
+
+    
