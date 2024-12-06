b09f817c25ef6e9a3d52d3df1ca1ee032de8047c
JTubo <joshuatubo4@gmail.com>
2024-12-06 17:03:28 +0800

    feat: added admin class

--- smsAssignment/dashboard/admin.py
+++ smsAssignment/dashboard/admin.py
@@ -3,3 +3,9 @@ from django.contrib import admin
 # Register your models here.
 # Import the Message model from the current app's models module.
 from .models import Message
+
+
+# Define a custom admin class for the Message model.
+class MessageAdmin(admin.ModelAdmin):
+    # Specify the fields to display in the list view of the admin panel.
+    list_display = ['team_name', 'event_name', 'event_date', 'event_time']  # Fixed field name here
