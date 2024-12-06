ff9c21e3097590b1ee107640213c3bf9e764327f
JTubo <joshuatubo4@gmail.com>
2024-12-06 17:04:12 +0800

    feat: added register the message model

--- smsAssignment/dashboard/admin.py
+++ smsAssignment/dashboard/admin.py
@@ -9,3 +9,6 @@ from .models import Message
 class MessageAdmin(admin.ModelAdmin):
     # Specify the fields to display in the list view of the admin panel.
     list_display = ['team_name', 'event_name', 'event_date', 'event_time']  # Fixed field name here
+
+# Register the Message model with the custom admin class to integrate it into the admin site.
+admin.site.register(Message, MessageAdmin)
