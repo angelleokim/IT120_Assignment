83970e8dcfcb005172e7db3b980f0174c419bcc8
chlintcuizon <chlint.cuizon@carsu.edu.ph>
2024-12-06 17:53:15 +0800

    added a migrations

--- smsAssignment/smsAssignment/settings.py
+++ smsAssignment/smsAssignment/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'dashboard.apps.DashboardConfig', # Custom application 'dashboard' is added to the project, using its specific app configuration class.
+    'dashboard.apps.DashboardConfig', # Custom application 'dashboard' is added to the project, using its specific app configuration class.
   
 ]
 
